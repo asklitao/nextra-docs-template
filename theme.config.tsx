79193496aef74cbb92a28f3a69122b3263cba153
realJackMoss <jacklitao.lt@gmail.com>
2026-02-09 13:51:59 +0800

    version 1.1

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© 2026 智驭星途 Stellar Drive',
   },
 }
 
